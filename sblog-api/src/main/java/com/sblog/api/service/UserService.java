b498713658eaa1476364ed54ce0868e603bc816d
e <491187718@qq.com>
2019-06-20 22:36:21 +0800

    生成实体类

--- sblog-api/src/main/java/com/sblog/api/service/UserService.java
+++ sblog-api/src/main/java/com/sblog/api/service/UserService.java
@@ -1,9 +1,62 @@
 package com.sblog.api.service;
 
+import com.sblog.common.dto.Response;
+import com.sblog.common.request.UserBody;
+import com.sblog.dao.entity.User;
+
 /**
  * @author ysj
  * @date 2019-06-20
  */
 public interface UserService {
     String get();
+
+    /**
+     * 登录
+     * @param username
+     * @param password
+     * @param rememberMe
+     * @return
+     */
+    Response login(String username, String password, Boolean rememberMe);
+
+    /**
+     * 注册
+     * @param userBody
+     * @return
+     */
+    Response register(UserBody userBody);
+
+
+    /**
+     * 验证用户名是否存在
+     * @param username
+     * @return
+     */
+    Response validUsername(String username);
+
+    /**
+     * 验证邮箱是否存在
+     * @param email
+     * @return
+     */
+    Response validEmail( String email);
+
+
+    /**
+     * 根据用户id获取用户信息
+     * @return
+     */
+    User selectById(String id);
+
+    /**
+     * 根据用户名获取用户
+     */
+    User selectUserByUsername(String username);
+
+    /**
+     * 更新用户信息
+     */
+    Response updateUser(User user);
+
 }
---
+++ sblog-common/src/main/java/com/sblog/common/dto/Response.java
@@ -0,0 +1,85 @@
+package com.sblog.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sblog.common.enums.ResponseEnum;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+/**
+ * 返回结果的封装
+ * status=0 表示成功
+ * @author yuan
+ * @param <T>
+ */
+@Getter
+public class Response<T> implements Serializable {
+
+    private Integer status;
+    private String msg;
+    private T data;
+
+    private Response(Integer status) {
+        this.status = status;
+    }
+
+    private Response(Integer status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private Response(Integer status, String msg, T data) {
+        this.status = status;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    private Response(Integer status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    /**
+     * JsonIgnore：使方法不在json序列号中，不返回给前台
+     * @return
+     */
+    @JsonIgnore
+    public boolean isSuccess() {
+        return this.status.equals(ResponseEnum.SUCCESS.getCode());
+    }
+
+
+    public static <T> Response<T> success(){
+        return new Response<T>(ResponseEnum.SUCCESS.getCode(), ResponseEnum.SUCCESS.getDesc());
+    }
+
+    public static <T> Response<T> successMsg(String msg){
+        return new Response<T>(ResponseEnum.SUCCESS.getCode(),msg);
+    }
+
+    public static <T> Response<T> success(T data){
+        return new Response<T>(ResponseEnum.SUCCESS.getCode(), ResponseEnum.SUCCESS.getDesc(), data);
+    }
+
+    public static <T> Response<T> successMsg(String msg, T data){
+        return new Response<T>(ResponseEnum.SUCCESS.getCode(),msg,data);
+    }
+
+    public static <T> Response<T> error() {
+        return new Response<>(ResponseEnum.ERROR.getCode(), ResponseEnum.ERROR.getDesc());
+    }
+
+    public static <T> Response<T> error(String errorMsg) {
+        return new Response<>(ResponseEnum.ERROR.getCode(), errorMsg);
+    }
+
+    public static <T> Response<T> errorMsg(Integer errorCode, String errorMsg) {
+        return new Response<>(errorCode, errorMsg);
+    }
+
+    public static <T> Response<T> errorMsg(ResponseEnum resultEnum) {
+        return new Response<>(resultEnum.getCode(), resultEnum.getDesc());
+    }
+
+
+}
